8c06372cebe33f05df46593ad712057b10ea208e
Mirco Bertelli <mircobe87@gmail.com>
2014-10-28 22:45:23 +0100

    aggiunta licenza

--- rgb.ino
+++ rgb.ino
@@ -1,3 +1,23 @@
+/*
+ * rgb.ino
+ * This file is part of arduino-nano-rgb
+ *
+ * Copyright (C) 2014 - Mirco Bertelli
+ *
+ * arduino-nano-rgb is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * arduino-nano-rgb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with arduino-nano-rgb. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Programma per Arduino Nano ATmega328.
  * Controlla il colore di un led RGB tramite un trimmer e un bottone.
